3977cd271bdc13edfb3b85acda96e701da560331
Changyi0416 <2651008688@qq.com>
2020-11-23 14:40:42 +0800

    新增了api接口文档

---
+++ back-admin/src/api/password.js
@@ -0,0 +1,23 @@
+import Server from "../utils/request"
+
+export function checkpassword(){
+    return Server({
+         url : "/user/pwd",
+        method : "POST",
+        data : {
+            userId : id,
+            password : oldPass
+        }
+   
+    })
+}
+export function updatePassword(id,newPass){
+    return Server({
+        url : "/user/pwd",
+        method : "PUT",
+        data : {
+            userId : id,
+            password : newPass
+        }
+    })
+}
