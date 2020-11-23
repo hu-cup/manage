551eb8370282e7b21d684003b5e36ef5930dd071
Changyi0416 <2651008688@qq.com>
2020-11-23 14:50:10 +0800

    修复了Layout文件中员工管理的路由错误

--- back-admin/src/components/Layout.vue
+++ back-admin/src/components/Layout.vue
@@ -29,7 +29,7 @@
                 <i class="el-icon-menu"></i>
                 <span slot="title">商品管理</span>
               </el-menu-item>
-              <el-menu-item index="merber">
+              <el-menu-item index="staff">
                 <i class="el-icon-menu"></i>
                 <span slot="title">员工管理</span>
               </el-menu-item>
