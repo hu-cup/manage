915ddeeb40f308b7269d935de50c1012c02a1c55
nyymr <3027912462@qq.com>
2020-11-23 19:42:23 +0800

    员工列表完成提交

--- back-admin/src/views/staff/index.vue
+++ back-admin/src/views/staff/index.vue
@@ -9,37 +9,134 @@
         <el-input v-model="formInline.name" placeholder="姓名"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">查询</el-button>
-        <el-button type="primary">新增</el-button>
-        <el-button>重置</el-button>
+        <el-button type="primary" @click="search">查询</el-button>
+        <el-button type="primary" @click="addShow = true">新增</el-button>
+        <el-button @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
-    <!-- 表格部分  :data="tableData"-->
-    <el-table border style="width: 100%">
-      <el-table-column prop="" label="序号" width="60"> </el-table-column>
-      <el-table-column prop="" label="账号" width="180"> </el-table-column>
-      <el-table-column prop="" label="姓名" width="80"> </el-table-column>
-      <el-table-column prop="" label="年龄" width="190"> </el-table-column>
-      <el-table-column prop="" label="电话" width="190"> </el-table-column>
-      <el-table-column prop="" label="薪酬" width="190"> </el-table-column>
-      <el-table-column prop="" label="入职时间" width="190"> </el-table-column>
+    <!-- 表格部分  -->
+    <el-table :data="list" border height="390" style="width: 100%">
+      <el-table-column prop="id" label="序号" width="60">
+        <template slot-scope="scope">
+          {{ scope.row.id - 9 }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="username" label="账号" width="180">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="80"> </el-table-column>
+      <el-table-column prop="age" label="年龄" width="190"> </el-table-column>
+      <el-table-column prop="mobile" label="电话" width="190">
+      </el-table-column>
+      <el-table-column prop="salary" label="薪酬" width="190">
+      </el-table-column>
+      <el-table-column prop="entryDate" label="入职时间" width="190">
+        <template slot-scope="scope">
+          {{ scope.row.entryDate | filterTime }}
+        </template>
+      </el-table-column>
       <el-table-column label="操作" width="190">
-        <template slot-scope="">
-          <el-button type="text" size="small">编辑</el-button>
-          <el-button type="danger" size="small">删除</el-button>
+        <template slot-scope="scope">
+          <el-button size="small" @click="edit(scope.row)">编辑</el-button>
+          <el-button type="danger" size="small" @click="remove(scope.$index)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
-    <!-- 分页器 @size-change="handleSizeChange"
-      @current-change="handleCurrentChange" -->
-    <el-pagination
-      :current-page="1"
-      :page-sizes="[10, 20, 50]"
-      :page-size="10"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="118"
-    >
-    </el-pagination>
+    <!-- 分页器  -->
+    <div style="margin: 16px">
+      <el-pagination
+        :current-page="pageNum"
+        :page-sizes="[10, 20, 50]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
+    <!-- 编辑弹框 -->
+    <el-dialog title="员工编辑" :visible.sync="editShow" width="30%">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input v-model="ruleForm.age"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="mobile">
+          <el-input v-model="ruleForm.mobile"></el-input>
+        </el-form-item>
+        <el-form-item label="薪酬" prop="salary">
+          <el-input v-model="ruleForm.salary"></el-input>
+        </el-form-item>
+        <el-form-item label="入职时间">
+          <el-form-item prop="entryDate">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="ruleForm.entryDate"
+              style="width: 60%"
+            ></el-date-picker>
+          </el-form-item>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editShow = false">取 消</el-button>
+        <el-button type="primary" @click="saveEdit(ruleForm.id)"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
+    <!-- 新增弹框 -->
+    <el-dialog title="添加员工" :visible.sync="addShow" width="30%">
+      <el-form
+        :model="addItems"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="账号" prop="username">
+          <el-input v-model="addItems.username"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="addItems.name"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄" prop="age">
+          <el-input v-model="addItems.age"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="mobile">
+          <el-input v-model="addItems.mobile"></el-input>
+        </el-form-item>
+        <el-form-item label="薪酬" prop="salary">
+          <el-input v-model="addItems.salary"></el-input>
+        </el-form-item>
+        <el-form-item label="入职时间">
+          <el-form-item prop="entryDate">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="addItems.entryDate"
+              style="width: 60%"
+            ></el-date-picker>
+          </el-form-item>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addShow = false">取 消</el-button>
+        <el-button type="primary" @click="saveAdd()">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -56,8 +153,46 @@ export default {
   data() {
     return {
       formInline: {
-        usre: "",
+        user: "",
         name: "",
+      },
+      pageNum: 1,
+      pageSize: 10,
+      list: [],
+      allList: [],
+      total: 0,
+      editShow: false,
+      addShow: false,
+      //修改的数据
+      ruleForm: {
+        id: "",
+        username: "",
+        name: "",
+        age: "",
+        mobile: "",
+        salary: "",
+        entryDate: "",
+      },
+      //新增数据
+      addItems: {
+        id: "",
+        username: "",
+        name: "",
+        age: "",
+        mobile: "",
+        salary: "",
+        entryDate: "",
+      },
+      //表单校验
+      rules: {
+        name: [
+          { required: true, message: "请输入姓名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        username: [
+          { required: true, message: "请输入账户名称", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在 3 到 8 个字符", trigger: "blur" },
+        ],
       },
     };
   },
@@ -66,61 +201,107 @@ export default {
   // 侦听器
   watch: {},
   // 组件方法
-  methods: {},
-  // 以下是生命周期钩子 注：没用到的钩子请自行删除
-  /**
-   * 在实例初始化之后，组件属性计算之前，如data属性等
-   */
-  beforeCreate() {},
-  /**
-   * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
-   */
-  created() {},
-  /**
-   * 在挂载开始之前被调用：相关的 render 函数首次被调用。
-   */
-  beforeMount() {},
-  /**
-   * el 被新创建的 vm.$ el 替换，并挂载到实例上去之后调用该钩子。
-   * 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.$ el 也在文档内。
-   */
-  mounted() {
-    console.log(6512);
-    getStaffList()
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
+  methods: {
+    //请求方法
+    getList() {
+      getStaffList(this.pageNum, this.pageSize)
+        .then((res) => {
+          console.log(res.data);
+          this.total = res.data.total;
+          this.list = res.data.rows;
+          this.allList = Object.assign([], res.data.rows);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    //条数切换
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.getList();
+    },
+    //页码切换
+    handleCurrentChange(val) {
+      this.pageNum = val;
+      this.getList();
+    },
+    //编辑回填数据
+    edit(val) {
+      this.editShow = true;
+      this.ruleForm = val;
+    },
+    //保存编辑
+    saveEdit(val) {
+      this.editShow = false;
+      this.list.forEach((item) => {
+        if (item.id == val.id) {
+          item = val;
+        }
       });
+    },
+    //删除
+    remove(index) {
+      this.$confirm("确认删除这条记录吗", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.list.splice(index, 1);
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+    //重置
+    reset() {
+      this.list = Object.assign([], this.allList);
+      this.formInline.user = "";
+      this.formInline.name = "";
+    },
+    //搜索
+    search() {
+      this.list = this.list.filter((item) => {
+        return (
+          item.username.includes(this.formInline.user) &&
+          item.name.includes(this.formInline.name)
+        );
+      });
+      console.log(this.list);
+    },
+    //新增
+    saveAdd() {
+      this.addItems.id = this.list[this.list.length - 1].id + 1;
+
+      this.list.unshift(this.addItems);
+      this.addShow = false;
+      this.addItems = [];
+    },
   },
-  /**
-   * 数据更新时调用，发生在虚拟 DOM 重新渲染和打补丁之前。
-   * 你可以在这个钩子中进一步地更改状态，这不会触发附加的重渲染过程。
-   */
-  beforeUpdate() {},
-  /**
-   * 由于数据更改导致的虚拟 DOM 重新渲染和打补丁，在这之后会调用该钩子。
-   * 当这个钩子被调用时，组件 DOM 已经更新，所以你现在可以执行依赖于 DOM 的操作。
-   */
-  updated() {},
-  /**
-   * keep-alive 组件激活时调用。 仅针对keep-alive 组件有效
-   */
-  activated() {},
-  /**
-   * keep-alive 组件停用时调用。 仅针对keep-alive 组件有效
-   */
-  deactivated() {},
-  /**
-   * 实例销毁之前调用。在这一步，实例仍然完全可用。
-   */
-  beforeDestroy() {},
-  /**
-   * Vue 实例销毁后调用。调用后，Vue 实例指示的所有东西都会解绑定，
-   * 所有的事件监听器会被移除，所有的子实例也会被销毁。
-   */
-  destroyed() {},
+
+  mounted() {
+    //挂载完成，请求数据
+    this.getList();
+  },
+  filters: {
+    //时间过滤器
+    filterTime(value) {
+      var time = new Date(value);
+      var year = time.getFullYear();
+      var month = time.getMonth() + 1;
+      var date = time.getDate();
+      return `${year}-${month
+        .toString()
+        .padStart(2, "0")}-${date.toString().padStart(2, "0")}`;
+    },
+  },
 };
 </script> 
 <style scoped>
